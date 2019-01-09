8c1b1c23cf3874704a9df7dc2be35c2d535ff5c8
naveen semwal <naveenrhtdm4ever@gmail.com>
2019-01-09 22:26:03 +0530

    Updated db cred

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import pyodbc
 server = 'kms-db.database.windows.net'
 database = 'kms_dev'
 username = 'kmsadmin@kms-db'
-password = 'Dotvik@98'
+password = 'Dotvik@98765'
 driver= '{SQL Server}'
 connectionString = 'DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password
 print(connectionString)
